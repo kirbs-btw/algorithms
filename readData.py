9ffee4d5ab4cbf841df45609ee453198aecf7d06
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2021-12-21 18:46:05 +0100

    Add files via upload

--- readData.py
+++ readData.py
@@ -1,5 +1,10 @@
 
 def readData():
+    """
+    reads the data file of the random grid algorithm
+
+    :return:
+    """
     data = open("data.txt", "r+").readlines()
     averageList = []
     count = 0
@@ -7,10 +12,12 @@ def readData():
     for i in data:
         count += 1
         i = i[0:-1]
-        print([i])
         if "size" in i or i == []:
             count -= 1
-            sum = sum / count
+            try:
+                sum = sum / count
+            except:
+                pass
             averageList.append(round(sum, 5))
             count = 0
             sum = 0
@@ -18,4 +25,17 @@ def readData():
             sum += float(i)
 
     print(averageList)
+    
+
+    #prints averageList in cut up chuncks to use it further in excel
+
+    for f in averageList:
+        m = ""
+        for l in str(f):
+            if l == ".":
+                l = ","
+            m = m + l
+        print(m)
+
+    return averageList
 readData()
