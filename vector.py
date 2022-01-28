58fbc7a49cebb299cf1f6d25be3fd7df2966aab6
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-01-28 17:19:27 +0100

    Add files via upload

--- vector.py
+++ vector.py
@@ -6,11 +6,20 @@ class vector3:
         self.x2 = x2
         self.x3 = x3
 
-def sumVector3(vector):
-    vectorSum = math.sqrt(vector.x1 ** 2 + vector.x2 ** 2 + vector.x3 ** 2)
-    return vectorSum
+    def print(self):
+        """
+        prints the vector3 because printing
+        :return:
+        """
 
-def calcVector3(vectorA, vectorB):
+        values = [self.x1, self.x2,  self.x3]
+        print(values)
+
+def sumVector3(vX):
+    vSum = math.sqrt(vX.x1 ** 2 + vX.x2 ** 2 + vX.x3 ** 2)
+    return vSum
+
+def calcVector3(vA, vB):
     """
 
     :param vectorA: vector A
@@ -18,50 +27,49 @@ def calcVector3(vectorA, vectorB):
     :return:
     """
 
-    c1 = vectorB.x1 - vectorA.x1
-    c2 = vectorB.x2 - vectorA.x2
-    c3 = vectorB.x3 - vectorA.x3
+    c1 = vB.x1 - vA.x1
+    c2 = vB.x2 - vA.x2
+    c3 = vB.x3 - vA.x3
 
-    vectorC = vector3(c1, c2, c3)
+    vC = vector3(c1, c2, c3)
 
-    print([vectorC.x1, vectorC.x2, vectorC.x3])
+    print([vC.x1, vC.x2, vC.x3])
 
-    return vectorC
+    return vC
 
-def addVector3(vectorA, vectorB):
+def addVector3(vA, vB):
     """
     Add two vectors
     a1 + b1
     a2 + b2
     a3 + b3
 
-    :param vectorA: vektor A
-    :param vectorB: vektor B
+    :param vA: vektor A
+    :param vB: vektor B
     :return:
     """
 
-    c1 = vectorB.x1 + vectorA.x1
-    c2 = vectorB.x2 + vectorA.x2
-    c3 = vectorB.x3 + vectorA.x3
+    c1 = vB.x1 + vA.x1
+    c2 = vB.x2 + vA.x2
+    c3 = vB.x3 + vA.x3
 
-    vectorC = vector3(c1, c2, c3)
+    vC = vector3(c1, c2, c3)
 
-    print([vectorC.x1, vectorC.x2, vectorC.x3])
-    return vectorC
+    print([vC.x1, vC.x2, vC.x3])
+    return vC
 
 def main():
     #len vector calc
-    vectorA = vector3(1, 4, 5)
-    print(sumVector3(vectorA))
+    vA = vector3(1, 4, 5)
+    print(sumVector3(vA))
 
     #calc vektor between two
-    vectorA = vector3(1, 0, 3)
-    vectorB = vector3(-10, 4, 2)
-    calcVector3(vectorA, vectorB)
+    vA = vector3(1, 0, 3)
+    vB = vector3(-10, 4, 2)
+    calcVector3(vA, vB)
 
     #add two vektors
-    addVector3(vectorA, vectorB)
-    pass
+    addVector3(vA, vB)
 
 if __name__ == '__main__':
     main()
