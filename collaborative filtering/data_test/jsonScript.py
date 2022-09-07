44001b793e59f27baaa107a0273d90e3cde2c481
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-09-07 18:18:59 +0200

    Add files via upload

---
+++ collaborative filtering/data_test/jsonScript.py
@@ -0,0 +1,29 @@
+import json
+
+"""
+your py file cannot be named json.py :)
+
+you get a double error and nothing works
+"""
+
+def createJson():
+    f = open("data.json", "w+")
+
+def main():
+    data = {
+        "a":4,
+        "b": 1,
+        "c":43,
+        "d":7
+        }
+
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    with open('data.json') as file:
+        dataSet = json.load(file)
+        print(dataSet)
+
+
+if __name__ == '__main__':
+    main()
